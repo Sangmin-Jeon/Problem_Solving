8ce5eb6c0613a6990feb048b8b8cd7fd9bbbf6c4
SangMin-Jeon <59474775+Sangmin-Jeon@users.noreply.github.com>
2023-09-21 23:04:38 +0900

    [level 2] Title: 타겟 넘버, Time: 31.58 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.swift
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.swift
@@ -2,20 +2,21 @@ import Foundation
 
 var cnt = 0
 func solution(_ numbers:[Int], _ target:Int) -> Int {
-    dfs(numbers, target, 0, 0)
+    dfs(numbers, target, 0, 0, [])
     
 }
 
-func dfs(_ numbers:[Int], _ target:Int, _ index: Int, _ sum: Int) -> Int {
+func dfs(_ numbers:[Int], _ target:Int, _ number: Int, _ index: Int, _ sum: [Int]) -> Int {
+    var sum = sum
+    sum.append(number)
     if index == numbers.count {
-        if sum == target {
+        if sum.reduce(0, +) == target {
             cnt += 1
         }
         return 0
     }
-
-    let left = dfs(numbers, target, index+1, sum + numbers[index])
-    let right = dfs(numbers, target, index+1, sum - numbers[index])
+    let left = dfs(numbers, target, numbers[index], index+1, sum)
+    let right = dfs(numbers, target, -numbers[index], index+1, sum)
     
     return cnt
 }
