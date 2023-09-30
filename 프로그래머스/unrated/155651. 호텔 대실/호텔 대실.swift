e950863b8df8cb6cb10afdcced4b82f2572226f0
SangMin-Jeon <59474775+Sangmin-Jeon@users.noreply.github.com>
2023-09-30 18:44:51 +0900

    [unrated] Title: 호텔 대실, Time: 122.22 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.swift
+++ 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.swift
@@ -2,35 +2,31 @@ import Foundation
 
 func solution(_ book_time:[[String]]) -> Int {
     var times = [(st: Int, et: Int)]()
-    book_time.forEach({ _times in
-        times.append((_times.first!.setTime(type: "st"), _times.last!.setTime(type: "et")))
-    })
+    for _time in book_time {
+        times.append((st: _time[0].cT(), et: _time[1].cT() + 10))
+    }
+    var rooms = [(st: Int, et: Int)]()
     times.sort(by: { $0.st < $1.st })
     
-    var rooms = [(st: Int, et: Int)]()
-
-    loop: for time in times {
+loop: for time in times {
         for (idx, room) in rooms.enumerated() {
             if !(room.st ..< room.et ~= time.st) {
                 rooms[idx] = time
                 continue loop
             }
         }
-
-        rooms.append((st: time.st, et: time.et))
+        
+        rooms.append((st: time.st ,et: time.et))
     }
 
-    
     return rooms.count
 }
 
-
 extension String {
-    func setTime(type: String) -> Int {
-        let components = self.split(separator: ":")
-        let hour = Int(components[0])!
-        let minute = Int(components[1])!
-        return type == "et" ? (hour * 60 + minute + 10) : (hour * 60 + minute)
+    func cT() -> Int {
+        let times = self.map({ String($0) })
+        let hour = Int(times[0] + times[1])! * 60
+        let min = Int(times[3] + times[4])!
+        return hour + min
     }
-    
 }
