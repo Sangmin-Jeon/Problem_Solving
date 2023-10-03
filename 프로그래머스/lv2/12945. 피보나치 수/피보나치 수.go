1ac8390b5b6753044cb4b9624d54a663240c14eb
SangMin-Jeon <59474775+Sangmin-Jeon@users.noreply.github.com>
2023-10-03 09:49:50 +0900

    [level 2] Title: 피보나치 수, Time: 32.10 ms, Memory: 18.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.go
@@ -0,0 +1,22 @@
+const(
+	fib0 = 0
+	fib1 = 1
+)
+var memo = make(map[int]int)
+
+func solution(n int) int {
+	memo[0] = 0
+	memo[1] = 1
+	return dfs(n)
+}
+
+func dfs(n int) int {
+	fib, exist := memo[n]
+	if exist {
+		return fib
+	}
+	
+	memo[n] = solution(n - 1) + solution(n - 2)
+	return (memo[n] % 1234567)
+
+}
