7a8489659720460c1add86037ec3d3f612ac2e4f
SangMin-Jeon <59474775+Sangmin-Jeon@users.noreply.github.com>
2023-10-16 19:11:17 +0900

    [Silver III] Title: N과 M (4), Time: 28 ms, Memory: 80140 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+let line = readLine()!.split(separator: " ").map { Int($0)! }
+let n = line[0]
+let m = line[1]
+
+var sequence = Array(1 ... n)
+var visited = Array(repeating: false, count: sequence.count)
+var list = [Int]()
+var answer = [String]()
+var memo = [[Int]]()
+
+NAndM(i: 0)
+for num in answer {
+    // 여기서 열거하면서 정답 출력
+    print(num)
+}
+
+func NAndM(i: Int) {
+    if list.count == m {
+        let result = list.map { String($0) }.joined(separator: " ")
+        answer.append(result)
+        return
+    }
+
+    for num in i ..< n {
+        list.append(sequence[num])
+        NAndM(i: num)
+        list.removeLast() 
+    }
+}
