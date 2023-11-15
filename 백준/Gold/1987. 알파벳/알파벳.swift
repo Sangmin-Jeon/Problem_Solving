560e38a5bd2decc36a26f23c94f9023e027f3d27
SangMin-Jeon <59474775+Sangmin-Jeon@users.noreply.github.com>
2023-11-15 18:03:00 +0900

    [Gold IV] Title: 알파벳, Time: 1292 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+let input = readLine()!.split(separator: " ").map({ Int($0)! })
+let row = input[0]
+let column = input[1]
+var graph = [[Int]]()
+for _ in 0 ..< row {
+    let _row = readLine()!.map({ Int($0.asciiValue!) - 65 })
+    graph.append(_row)
+}
+
+var answer = 0
+var route = Array(repeating: false, count: 100)
+route[Int(UnicodeScalar(graph[0][0])!.value)] = true
+let dr = [1, 0, -1, 0]
+let dc = [0, 1, 0, -1]
+
+let mask = 1 << graph[0][0]
+dfs(&route, mask, (r: 0, c: 0), 1)
+print(answer)
+
+func dfs(_ route: inout [Bool],
+         _ alphaMask: Int,
+         _ dist: (r: Int, c: Int),
+         _ depth: Int) {
+
+    answer = max(answer, depth)
+
+    for i in 0 ..< 4 {
+        let r = dist.r + dr[i]
+        let c = dist.c + dc[i]
+        
+        if r >= 0, c >= 0, row > r, column > c {
+            let ascii = 1 << graph[r][c]
+            if alphaMask & ascii == 0 {
+                dfs(&route, alphaMask | ascii, (r: r, c: c), depth + 1)
+                
+            }
+        }
+    }
+
+}
