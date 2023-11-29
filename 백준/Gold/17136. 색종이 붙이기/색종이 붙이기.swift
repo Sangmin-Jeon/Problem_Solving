8904e5f0ea363581770a0fc3b218341a13e3ef97
SangMin-Jeon <59474775+Sangmin-Jeon@users.noreply.github.com>
2023-11-29 16:53:22 +0900

    [Gold II] Title: 색종이 붙이기, Time: 80 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.swift
@@ -0,0 +1,86 @@
+import Foundation
+
+var paper = [Int](repeating: 5, count: 5)
+var result = 100
+var graph = [[Int]]()
+
+func canAttach(r: Int, c: Int, n: Int) -> Bool {
+    if r + n >= 10 || c + n >= 10 {
+        return false
+    }
+    for _r in r ... r + n {
+        for _c in c ... c + n {
+            if graph[_r][_c] == 0 {
+                return false
+            }
+        }
+    }
+    return true
+}
+
+func checkPaper() -> Bool {
+    for r in 0 ..< 10 {
+        for c in 0 ..< 10 {
+            if graph[r][c] == 1 {
+                return false
+            }
+        }
+    }
+    return true
+}
+
+func fillPaper(r: Int, c: Int, n: Int, f: Int) {
+    for _r in r ... r + n {
+        for _c in c ... c + n {
+            graph[_r][_c] = f
+        }
+    }
+}
+
+func range_check(r: Int, c: Int) -> Bool {
+	if r < 10 && c < 10 {
+        return true   
+    }
+	return false
+}
+
+
+func attachPaper(pCnt: Int) {
+    if result < pCnt {
+        return
+    }
+    
+    if checkPaper() {
+        if result > pCnt {
+            result = pCnt // 최소값 저장
+            return
+        }
+    }
+
+    for r in 0 ..< 10 {
+        for c in 0 ..< 10 {
+            if graph[r][c] == 1 {
+                for n in (0...4).reversed() {
+                    if paper[n] > 0 && canAttach(r: r, c: c, n: n) && range_check(r: r + n, c: c + n) {
+                        paper[n] -= 1
+                        fillPaper(r: r, c: c, n: n, f: 0)
+                        attachPaper(pCnt: pCnt + 1)
+                        paper[n] += 1
+                        fillPaper(r: r, c: c, n: n, f: 1)
+                    }
+                }
+                return
+            }
+        }
+    }
+}
+
+// input
+for _ in 0 ..< 10 {
+    let row = readLine()!.split(separator: " ").map({ Int($0)! })
+    graph.append(row)
+}
+
+attachPaper(pCnt: 0)
+print(result == 100 ? -1 : result)
+
