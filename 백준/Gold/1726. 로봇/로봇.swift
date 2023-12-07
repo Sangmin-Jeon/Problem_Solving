ef9b2973a8e3cfd2e7f6c2750dee0751d9ce0eb1
SangMin-Jeon <59474775+Sangmin-Jeon@users.noreply.github.com>
2023-12-07 14:23:47 +0900

    [Gold III] Title: 로봇, Time: 16 ms, Memory: 79648 KB -BaekjoonHub

--- 백준/Gold/1726. 로봇/로봇.swift
+++ 백준/Gold/1726. 로봇/로봇.swift
@@ -1,20 +1,16 @@
 import Foundation
 
-// 입력 처리 함수
-func readLineToArray() -> [Int] {
-    return readLine()!.split(separator: " ").map { Int($0)! }
-}
-
 func bfs(s_y: Int, s_x: Int, s_d: Int) -> Int {
     var visited = Array(repeating: Array(repeating: Array(repeating: 0, count: 4), count: m), count: n)
     visited[s_y][s_x][s_d] = 1
-    var q = [(s_y, s_x, s_d, 0)]
-    var index = 0
+    var queue = [(s_y, s_x, s_d, 0)]
 
-    while index < q.count {
-        let (y, x, d, cnt) = q[index]
-        index += 1
-        if y == a_y && x == a_x && d == a_d { return cnt }
+    while !queue.isEmpty {
+        let (y, x, d, cnt) = queue.removeFirst()
+
+        if y == a_y && x == a_x && d == a_d { 
+            return cnt 
+        }
 
         for step in 1...3 {
             let dy = y + my[d] * step
@@ -22,18 +18,22 @@ func bfs(s_y: Int, s_x: Int, s_d: Int) -> Int {
             if dy < 0 || dy >= n || dx < 0 || dx >= m || matrix[dy][dx] == 1 { break }
             if visited[dy][dx][d] == 0 {
                 visited[dy][dx][d] = 1
-                q.append((dy, dx, d, cnt + 1))
+                queue.append((dy, dx, d, cnt + 1))
             }
         }
 
         for r_d in rotate[d]! {
             if visited[y][x][r_d] == 0 {
                 visited[y][x][r_d] = 1
-                q.append((y, x, r_d, cnt + 1))
+                queue.append((y, x, r_d, cnt + 1))
             }
         }
     }
     return -1
+}
+
+func readLineToArray() -> [Int] {
+    readLine()!.split(separator: " ").map { Int($0)! }
 }
 
 let inputs = readLineToArray()
@@ -62,3 +62,4 @@ let a_x = end[1] - 1
 let a_d = end[2] - 1
 
 print(bfs(s_y: s_y, s_x: s_x, s_d: s_d))
+
