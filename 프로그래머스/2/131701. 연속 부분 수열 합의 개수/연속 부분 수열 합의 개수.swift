b8fe382a733b116621e9630351cb8f4441c5c3e5
SangMin-Jeon <59474775+Sangmin-Jeon@users.noreply.github.com>
2024-01-19 13:47:24 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 192.04 ms, Memory: 27.5 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.swift
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.swift
@@ -1,25 +1,26 @@
 import Foundation
 
 func solution(_ elements: [Int]) -> Int {
-    let cnt = elements.count
-    var sums = Array(repeating: 0, count: cnt + 1)
-    var answer = Set<Int>()
+    let 원소들 = elements
+    let 총원소수 = 원소들.count
+    var 누적합 = Array(repeating: 0, count: 총원소수 + 1)
+    var 정답 = Set<Int>()
 
-    for i in 0 ..< cnt {
-        sums[i + 1] = sums[i] + elements[i]
+    for 인덱스 in 0 ..< 총원소수 {
+        누적합[인덱스 + 1] = 누적합[인덱스] + 원소들[인덱스]
     }
 
-    for len in 1...cnt {
-        for s in 0 ..< cnt {
-            var e = s + len
-            if e > cnt {
-                e -= cnt
-                answer.insert(sums[cnt] - sums[s] + sums[e])
+    for 길이 in 1...총원소수 {
+        for 시작 in 0 ..< 총원소수 {
+            var 끝 = 시작 + 길이
+            if 끝 > 총원소수 {
+                끝 -= 총원소수
+                정답.insert(누적합[총원소수] - 누적합[시작] + 누적합[끝])
             } else {
-                answer.insert(sums[e] - sums[s])
+                정답.insert(누적합[끝] - 누적합[시작])
             }
         }
     }
 
-    return answer.count
+    return 정답.count
 }
