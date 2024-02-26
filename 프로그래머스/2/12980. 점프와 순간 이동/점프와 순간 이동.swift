4bf29754525f99c9693dcaaac8bfac32411e9761
SangMin-Jeon <59474775+Sangmin-Jeon@users.noreply.github.com>
2024-02-26 23:39:23 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.swift
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.swift
@@ -2,9 +2,9 @@ import Foundation
 
 func solution(_ n:Int) -> Int {
     var n = n
-    var answer = 1
+    var answer = 0
 
-    while n > 1 {
+    while n > 0 {
         if n % 2 != 0 {
             answer += 1
         }
