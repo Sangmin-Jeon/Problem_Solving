c9059e52cf4774a53a6fc580cbd0ba18c07933a2
SangMin-Jeon <59474775+Sangmin-Jeon@users.noreply.github.com>
2024-03-12 16:19:19 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.00 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+
+func solution(_ storey:Int) -> Int {
+    var storey = storey
+    var result = 0
+    
+    while storey != 0 {
+        let n = storey % 10
+        
+        if n >= 6 || (n == 5 && (storey / 10) % 10 >= 5) {
+            storey += 10 - n
+            result += 10 - n
+        } else {
+            result += n
+        }
+        storey /= 10
+    }
+    
+    return result
+}
+
+
+
+
