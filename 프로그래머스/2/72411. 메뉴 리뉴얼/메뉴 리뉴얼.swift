443c5d5876491dcbda81d509f11679a1d6a73490
SangMin-Jeon <59474775+Sangmin-Jeon@users.noreply.github.com>
2024-03-14 20:44:32 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 24.53 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.swift
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.swift
@@ -1,14 +1,76 @@
 import Foundation
 
-func getTotalOrders(_ orders: [String]) -> [String] {
-    var setOrders = Set<String>()
-    orders.forEach { order in
-        order.forEach { item in
-            setOrders.insert(String(item))
-        }
-    }
-    return Array(setOrders).sorted()
-}
+// // 전체 단품 내역 리스트 구하기
+// func getTotalOrders(_ orders:[String]) -> [String] {
+//     var setOrders = Set<String>()
+//     for order in orders {
+//         order.forEach({ 
+//             setOrders.insert(String($0))  
+//         })
+//     }
+//     return [String](setOrders)
+// }
+
+// // 코스 요리 수에 따른 조합 구하기
+// func combination(_ totalOrders: [String], _ index: Int, _ combi:[String], _ target: Int, _ list: inout [[String]]) {
+//     if combi.count == target {
+//         list.append(combi)
+//         return
+//     }
+//     for i in index ..< totalOrders.count {
+//         combination(totalOrders, i + 1, combi + [totalOrders[i]], target, &list)
+//     }
+// }
+
+// func solution(_ orders:[String], _ course:[Int]) -> [String] {
+//     var answer = [String]()
+//     let totalOrders = getTotalOrders(orders)
+//     for target in course {
+//         var list = [[String]]()
+//         combination(totalOrders, 0, [], target, &list)
+//         for _course in makeCourse(orders, list, target) {
+//             answer.append(_course)   
+//         }
+//     }
+//     answer.sort()
+    
+//     return answer
+// }
+
+// func makeCourse(_ orders: [String], _ list: [[String]], _ target: Int) -> [String] {
+//     var answer = [[String]]()
+    
+//     var list = list
+    
+//     var hash = [String: Int]()
+    
+//     while !list.isEmpty {
+//         var pop = list.removeFirst()
+//         var count = 0
+//         for order in orders {
+//             let temp = order.map({ String($0) })
+//             var hit = 0
+//             for elem in pop {
+//                 if temp.contains(elem) {
+//                     hit += 1
+//                 }
+//             }
+//             if hit == target {
+//                 count += 1
+//             }
+//         }
+//         if count >= 2 {
+//             pop.sort()
+//             hash[pop.reduce("", +), default: 0] = count
+//         }
+//     }
+
+//     let maxVal = hash.values.max()
+    
+//     return hash.filter({ $0.value == maxVal }).map({ $0.key })
+// }
+
+
 
 func getCombinationCounts(_ orders: [String], _ courseSize: Int) -> [String: Int] {
     var counts = [String: Int]()
@@ -46,3 +108,9 @@ func solution(_ orders: [String], _ course: [Int]) -> [String] {
     
     return answer.sorted()
 }
+
+
+
+
+
+
