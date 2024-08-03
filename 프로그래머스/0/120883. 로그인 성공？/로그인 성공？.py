128d60b49ac444820eb5cfe5f79ec504f1a71590
SangMin-Jeon <59474775+Sangmin-Jeon@users.noreply.github.com>
2024-08-03 15:21:30 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -0,0 +1,9 @@
+def solution(id_pw, db):
+    answer = 'fail'
+    for user in db:
+        if id_pw[0] == user[0]:
+            if id_pw[1] == user[1]:
+                answer = "login"
+            else:
+                answer = "wrong pw"
+    return answer
