55af5b4eec07c3f9fde05c1192672e2eb0fb992a
SangMin-Jeon <59474775+Sangmin-Jeon@users.noreply.github.com>
2024-08-03 18:50:32 +0900

    [level 1] Title: 소수 만들기, Time: 18.59 ms, Memory: 12.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,22 @@
+from itertools import *
+
+def solution(nums):
+    comb = [sum(num) for num in list(combinations(nums, 3))]
+    
+    def is_prime(num):
+        if num <= 1:
+            return False
+        if num <= 3:
+            return True
+        if num % 2 == 0 or num % 3 == 0:
+            return False
+        i = 5
+        while i * i <= num:
+            if num % i == 0 or num % (i + 2) == 0:
+                return False
+            i += 6
+        return True
+    
+    prime_num = list(filter(lambda num: is_prime(num), comb))
+    
+    return len(prime_num)
