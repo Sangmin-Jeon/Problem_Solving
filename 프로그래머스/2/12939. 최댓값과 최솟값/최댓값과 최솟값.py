72d17dec2fa9aed5b158702d2f3e4e630f0d9aa7
SangMin-Jeon <59474775+Sangmin-Jeon@users.noreply.github.com>
2024-08-04 21:43:34 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,3 +1,5 @@
 def solution(s):
-    nums = list(map(lambda s: int(s[1:]) * -1 if s[0] == "-" else int(s), s.split(" ")))
+    nums = list(map(lambda s: int(s[1:]) * -1 \
+                    if s[0] == "-" \
+                    else int(s), s.split(" ")))
     return str(min(nums)) + ' ' + str(max(nums))
