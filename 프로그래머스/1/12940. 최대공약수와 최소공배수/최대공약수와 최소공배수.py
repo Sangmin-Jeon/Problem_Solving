d2a0ce4e6e30b93cf2d28a14589b84a0cba5ff11
SangMin-Jeon <59474775+Sangmin-Jeon@users.noreply.github.com>
2024-08-06 11:03:10 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,10 +1,7 @@
+import math
+
 def solution(n, m):
-    gcd = cal_gcd(n, m)
-    lcm = lambda n, m, gcd: int(n * m // gcd)
-    return [gcd, lcm(n, m, gcd)]
-
-def cal_gcd(n, m):
-    if m == 0:
-        return n
-    return cal_gcd(m, n % m)
-
+    answer = []
+    gcd = math.gcd(n, m)
+    lcm = n * m // gcd
+    return [gcd, lcm]
